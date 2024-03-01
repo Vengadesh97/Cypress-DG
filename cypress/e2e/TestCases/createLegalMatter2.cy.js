3b498364b04449f8c23e878f6fdcf7e37fb9bab9
Vengadesh <vengadeshw45@gmail.com>
2024-03-01 17:05:17 +0530

    Code Updated

--- cypress/e2e/TestCases/createLegalMatter2.cy.js
+++ cypress/e2e/TestCases/createLegalMatter2.cy.js
@@ -411,12 +411,13 @@ describe("Create Legal Matter Scenario 3",()=>{
 
 
     it("8.Mandatory DOF and Description with Departments", () => {
-        create.leftSideMatterTab();
         create.createTab();
-        create.caseTitleField(data.text1.caseTitle);
-        create.caseNumberField(data.text1.caseNumber);
+        create.caseTitleField(data.text8.caseTitle);
+        create.caseNumberField(data.text8.caseNumber);
         create.scrollDown();
-  
+        create.dateClick();
+        create.dateOfFilling(data.text8.dateFill);
+        create.descriptionField(data.text8.description);
         create.nextButton();
         //Departments
         create.selectOnRightSide(selectDepartments);
@@ -431,24 +432,26 @@ describe("Create Legal Matter Scenario 3",()=>{
         create.confirmationYesButton();
         //Success popup
         create.successViewMatterButton();
-        view.viewMatterSearchBox(data.text1.caseTitle);
-        view.actionButtonsClick(data.text1.caseTitle);
+        view.viewMatterSearchBox(data.text8.caseTitle);
+        view.actionButtonsClick(data.text8.caseTitle);
         view.editMatterInfoMenuButton();
-        view.verifyEditMatterInfoCaseTitle(data.text1.caseTitle);
-        view.verifyEditMatterInfoCaseNumber(data.text1.caseNumber);
+        view.verifyEditMatterInfoCaseTitle(data.text8.caseTitle);
+        view.verifyEditMatterInfoCaseNumber(data.text8.caseNumber);
+        view.verifyEditMatterInfoDescription(data.text8.Description);
         view.editMatterInfoCancelButton();
-        view.actionButtonsClick(data.text1.caseTitle);
+        view.actionButtonsClick(data.text8.caseTitle);
         view.updateDepartmentMenuButton();
         view.updateDepartmentCheckSelectedDepartmentName(selectDepartments);
     })
 
     it("9.Mandatory DOF and Description with Departments and Clients", () => {
         create.createTab();
-        create.caseTitleField(data.text2.caseTitle);
-        create.caseNumberField(data.text2.caseNumber);
+        create.caseTitleField(data.text9.caseTitle);
+        create.caseNumberField(data.text9.caseNumber);
         create.scrollDown();
-        create.opponentAdvocateAddButton();
-       
+        create.dateClick();
+        create.dateOfFilling(data.text9.dateFill);
+        create.descriptionField(data.text9.description);
         create.nextButton();
         //Departments
         create.selectOnRightSide(selectDepartments);
@@ -465,9 +468,9 @@ describe("Create Legal Matter Scenario 3",()=>{
         //Success Popup
         create.successViewMatterButton();
         //View Matter 
-        view.viewMatterSearchBox(data.text2.caseTitle);
+        view.viewMatterSearchBox(data.text9.caseTitle);
         // Action Button
-        view.actionButtonsClick(data.text2.caseTitle);
+        view.actionButtonsClick(data.text9.caseTitle);
         // View Details
         view.viewDetailsMenuButton();
         // Team Member and Client Tab
@@ -478,14 +481,15 @@ describe("Create Legal Matter Scenario 3",()=>{
         view.viewDetailsCheckSelectedTeamMemberOrClientNameAppear(checkSelectedClient);
         create.scrollUp();
         view.viewTab();
-        view.viewMatterSearchBox(data.text2.caseTitle);
-        view.actionButtonsClick(data.text2.caseTitle);
+        view.viewMatterSearchBox(data.text9.caseTitle);
+        view.actionButtonsClick(data.text9.caseTitle);
         // Edit Matter Info
         view.editMatterInfoMenuButton();
-        view.verifyEditMatterInfoCaseTitle(data.text2.caseTitle);
-        view.verifyEditMatterInfoCaseNumber(data.text2.caseNumber);
+        view.verifyEditMatterInfoCaseTitle(data.text9.caseTitle);
+        view.verifyEditMatterInfoCaseNumber(data.text9.caseNumber);
+        view.verifyEditMatterInfoDescription(data.text9.Description);
         view.editMatterInfoCancelButton();
-        view.actionButtonsClick(data.text2.caseTitle);
+        view.actionButtonsClick(data.text9.caseTitle);
         // Update Department
         view.updateDepartmentMenuButton();
         view.updateDepartmentCheckSelectedDepartmentName(selectDepartments);
@@ -493,10 +497,12 @@ describe("Create Legal Matter Scenario 3",()=>{
 
     it("10.Mandatory DOF and Description with Departments and TM", () => {
         create.createTab();
-        create.caseTitleField(data.text3.caseTitle);
-        create.caseNumberField(data.text3.caseNumber);
+        create.caseTitleField(data.text10.caseTitle);
+        create.caseNumberField(data.text10.caseNumber);
         create.scrollDown();
-     
+        create.dateClick();
+        create.dateOfFilling(data.text10.dateFill);
+        create.descriptionField(data.text10.description);
         create.nextButton();
         // Departments
         create.selectOnRightSide(selectDepartments);
@@ -513,9 +519,9 @@ describe("Create Legal Matter Scenario 3",()=>{
         // Success Popup
         create.successViewMatterButton();
         // View Matter 
-        view.viewMatterSearchBox(data.text3.caseTitle);
+        view.viewMatterSearchBox(data.text10.caseTitle);
         // Action Button
-        view.actionButtonsClick(data.text3.caseTitle);
+        view.actionButtonsClick(data.text10.caseTitle);
         // View Details
         view.viewDetailsMenuButton();
         // Team Member and Client Tab
@@ -526,14 +532,15 @@ describe("Create Legal Matter Scenario 3",()=>{
         view.viewDetailsCheckUnSelectedTeamMemberOrClientName(checkUnSelectMembers);
         create.scrollUp();
         view.viewTab();
-        view.viewMatterSearchBox(data.text3.caseTitle);
-        view.actionButtonsClick(data.text3.caseTitle);
+        view.viewMatterSearchBox(data.text10.caseTitle);
+        view.actionButtonsClick(data.text10.caseTitle);
         // Edit Matter Info
         view.editMatterInfoMenuButton();
-        view.verifyEditMatterInfoCaseTitle(data.text3.caseTitle);
-        view.verifyEditMatterInfoCaseNumber(data.text3.caseNumber);
+        view.verifyEditMatterInfoCaseTitle(data.text10.caseTitle);
+        view.verifyEditMatterInfoCaseNumber(data.text10.caseNumber);
+        view.verifyEditMatterInfoDescription(data.text10.Description);
         view.editMatterInfoCancelButton();
-        view.actionButtonsClick(data.text3.caseTitle);
+        view.actionButtonsClick(data.text10.caseTitle);
         // Update Department
         view.updateDepartmentMenuButton();
         view.updateDepartmentCheckSelectedDepartmentName(selectDepartments);
@@ -541,10 +548,12 @@ describe("Create Legal Matter Scenario 3",()=>{
 
     it("11.Mandatory DOF and Description with Departments and Document ", () => {
         create.createTab();
-        create.caseTitleField(data.text4.caseTitle);
-        create.caseNumberField(data.text4.caseNumber);
+        create.caseTitleField(data.text11.caseTitle);
+        create.caseNumberField(data.text11.caseNumber);
         create.scrollDown();
-      
+        create.dateClick();
+        create.dateOfFilling(data.text11.dateFill);
+        create.descriptionField(data.text11.description);
         create.nextButton();
         //Departments
         create.selectOnRightSide(selectDepartments);
@@ -554,38 +563,37 @@ describe("Create Legal Matter Scenario 3",()=>{
         //Team Members
         create.commonNextButton();
         //Documents
-        create.selectOnRightSide(selectDocuments);
+        create.selectOnRightSide(data.text11.document1);
         create.submitButton();
         //Confirmation Popup
         create.confirmationYesButton();
         //Success Popup
         create.successViewMatterButton();
         //View Matter 
-        view.viewMatterSearchBox(data.text4.caseTitle);
-        view.actionButtonsClick(data.text4.caseTitle);
+        view.viewMatterSearchBox(data.text11.caseTitle);
+        view.actionButtonsClick(data.text11.caseTitle);
         // View Details
         view.viewDetailsMenuButton();
         // Document Tab
         view.viewDetailsDocumentTab();
         // Check Selected Document Name Appear
-        view.viewDetailsCheckSelectedDocumentNameAppear(checkSelectedDocuments);
+        view.viewDetailsCheckSelectedDocumentNameAppear(data.text11.document1);
         // Add New + Tab
         view.addNewTab();
         // Check Selected Document Name Appear
-        view.viewDetailsAddExistingCheckSelectedDocumentNameAppear(checkSelectedDocuments);
-        // Check UnSelected Document Name Appear
-        view.viewDetailsAddExistingCheckUnSelectedDocumentNameAppear(checkUnSelectedDocuments);
+        view.viewDetailsAddExistingCheckSelectedDocumentNameAppear(data.text11.document1);
         create.scrollUp();
         view.viewTab();
-        view.viewMatterSearchBox(data.text4.caseTitle);
+        view.viewMatterSearchBox(data.text11.caseTitle);
         // Action Button
-        view.actionButtonsClick(data.text4.caseTitle);
+        view.actionButtonsClick(data.text11.caseTitle);
         // Edit Matter Info
         view.editMatterInfoMenuButton();
-        view.verifyEditMatterInfoCaseTitle(data.text4.caseTitle);
-        view.verifyEditMatterInfoCaseNumber(data.text4.caseNumber);
+        view.verifyEditMatterInfoCaseTitle(data.text11.caseTitle);
+        view.verifyEditMatterInfoCaseNumber(data.text11.caseNumber);
+        view.verifyEditMatterInfoDescription(data.text11.Description);
         view.editMatterInfoCancelButton();
-        view.actionButtonsClick(data.text4.caseTitle);
+        view.actionButtonsClick(data.text11.caseTitle);
         // Update Department
         view.updateDepartmentMenuButton();
         view.updateDepartmentCheckSelectedDepartmentName(selectDepartments);
@@ -593,10 +601,12 @@ describe("Create Legal Matter Scenario 3",()=>{
 
     it("12.Mandatory DOF and Description with Departments ,Clients and TM ", () => {
         create.createTab();
-        create.caseTitleField(data.text5.caseTitle);
-        create.caseNumberField(data.text5.caseNumber);
+        create.caseTitleField(data.text12.caseTitle);
+        create.caseNumberField(data.text12.caseNumber);
         create.scrollDown();
-     
+        create.dateClick();
+        create.dateOfFilling(data.text12.dateFill);
+        create.descriptionField(data.text12.description);
         create.nextButton();
         //Departments
         create.selectOnRightSide(selectDepartments);
@@ -613,11 +623,10 @@ describe("Create Legal Matter Scenario 3",()=>{
         create.confirmationYesButton();
         //Success Popup
         create.successViewMatterButton();
-
         //View Matter 
-        view.viewMatterSearchBox(data.text5.caseTitle);
+        view.viewMatterSearchBox(data.text12.caseTitle);
         // Action Button
-        view.actionButtonsClick(data.text5.caseTitle);
+        view.actionButtonsClick(data.text12.caseTitle);
         // View Details
         view.viewDetailsMenuButton();
         // Team Member and Client Tab
@@ -632,14 +641,15 @@ describe("Create Legal Matter Scenario 3",()=>{
         view.viewDetailsCheckSelectedTeamMemberOrClientNameAppear(checkSelectedClient);
         create.scrollUp();
         view.viewTab();
-        view.viewMatterSearchBox(data.text5.caseTitle);
-        view.actionButtonsClick(data.text5.caseTitle);
+        view.viewMatterSearchBox(data.text12.caseTitle);
+        view.actionButtonsClick(data.text12.caseTitle);
         // Edit Matter Info
         view.editMatterInfoMenuButton();
-        view.verifyEditMatterInfoCaseTitle(data.text5.caseTitle);
-        view.verifyEditMatterInfoCaseNumber(data.text5.caseNumber);
+        view.verifyEditMatterInfoCaseTitle(data.text12.caseTitle);
+        view.verifyEditMatterInfoCaseNumber(data.text12.caseNumber);
+        view.verifyEditMatterInfoDescription(data.text12.Description);
         view.editMatterInfoCancelButton();
-        view.actionButtonsClick(data.text5.caseTitle);
+        view.actionButtonsClick(data.text12.caseTitle);
         // Update Department
         view.updateDepartmentMenuButton();
         view.updateDepartmentCheckSelectedDepartmentName(selectDepartments);
@@ -647,10 +657,12 @@ describe("Create Legal Matter Scenario 3",()=>{
 
     it("13.Mandatory DOF and Description with Departments , Clients and Documents ", () => {
         create.createTab();
-        create.caseTitleField(data.text6.caseTitle);
-        create.caseNumberField(data.text6.caseNumber);
+        create.caseTitleField(data.text13.caseTitle);
+        create.caseNumberField(data.text13.caseNumber);
         create.scrollDown();
-   
+        create.dateClick();
+        create.dateOfFilling(data.text13.dateFill);
+        create.descriptionField(data.text13.description);
         create.nextButton();
         //Departments
         create.selectOnRightSide(selectDepartments);
@@ -661,27 +673,25 @@ describe("Create Legal Matter Scenario 3",()=>{
         //Team Members
         create.commonNextButton();
         //Documents
-        create.selectOnRightSide(selectDocuments);
+        create.selectOnRightSide(data.text13.document1);
         create.submitButton();
         //Confirmation Popup
         create.confirmationYesButton();
         //Success Popup
         create.successViewMatterButton();
         //View Matter 
-        view.viewMatterSearchBox(data.text6.caseTitle);
-        view.actionButtonsClick(data.text6.caseTitle);
+        view.viewMatterSearchBox(data.text13.caseTitle);
+        view.actionButtonsClick(data.text13.caseTitle);
         // View Details
         view.viewDetailsMenuButton();
         // Document Tab
         view.viewDetailsDocumentTab();
         // Check Selected Document Name Appear
-        view.viewDetailsCheckSelectedDocumentNameAppear(checkSelectedDocuments);
+        view.viewDetailsCheckSelectedDocumentNameAppear(data.text13.document1);
         // Add New + Tab
         view.addNewTab();
         // Check Selected Document Name Appear
-        view.viewDetailsAddExistingCheckSelectedDocumentNameAppear(checkSelectedDocuments);
-        // Check UnSelected Document Name Appear
-        view.viewDetailsAddExistingCheckUnSelectedDocumentNameAppear(checkUnSelectedDocuments);
+        view.viewDetailsAddExistingCheckSelectedDocumentNameAppear(data.text13.document1);
         // Team Member and Client Tab
         view.viewDetailsTeamMemberAndExternalCounselTab();
         //Client Tab
@@ -690,14 +700,15 @@ describe("Create Legal Matter Scenario 3",()=>{
         view.viewDetailsCheckSelectedTeamMemberOrClientNameAppear(checkSelectedClient);
         create.scrollUp();
         view.viewTab();
-        view.viewMatterSearchBox(data.text6.caseTitle);
-        view.actionButtonsClick(data.text6.caseTitle);
+        view.viewMatterSearchBox(data.text13.caseTitle);
+        view.actionButtonsClick(data.text13.caseTitle);
         // Edit Matter Info
         view.editMatterInfoMenuButton();
-        view.verifyEditMatterInfoCaseTitle(data.text6.caseTitle);
-        view.verifyEditMatterInfoCaseNumber(data.text6.caseNumber);
+        view.verifyEditMatterInfoCaseTitle(data.text13.caseTitle);
+        view.verifyEditMatterInfoCaseNumber(data.text13.caseNumber);
+        view.verifyEditMatterInfoDescription(data.text13.Description);
         view.editMatterInfoCancelButton();
-        view.actionButtonsClick(data.text6.caseTitle);
+        view.actionButtonsClick(data.text13.caseTitle);
         // Update Department
         view.updateDepartmentMenuButton();
         view.updateDepartmentCheckSelectedDepartmentName(selectDepartments);
@@ -705,10 +716,12 @@ describe("Create Legal Matter Scenario 3",()=>{
 
     it("14.Mandatory DOF and Description with All ", () => {
         create.createTab();
-        create.caseTitleField(data.text7.caseTitle);
-        create.caseNumberField(data.text7.caseNumber);
+        create.caseTitleField(data.text14.caseTitle);
+        create.caseNumberField(data.text14.caseNumber);
         create.scrollDown();
-     
+        create.dateClick();
+        create.dateOfFilling(data.text14.dateFill);
+        create.descriptionField(data.text14.description);
         create.nextButton();
         //Departments
         create.selectOnRightSide(selectDepartments);
@@ -720,28 +733,26 @@ describe("Create Legal Matter Scenario 3",()=>{
         create.selectOnRightSide(selectTeamMembers);
         create.commonNextButton();
         //Documents
-        create.selectOnRightSide(selectDocuments);
+        create.selectOnRightSide(data.text14.document1);
         create.submitButton();
         //Confirmation Popup
         create.confirmationYesButton();
         //Success Popup
         create.successViewMatterButton();
         //View Matter 
-        view.viewMatterSearchBox(data.text7.caseTitle);
+        view.viewMatterSearchBox(data.text14.caseTitle);
         // Action Button
-        view.actionButtonsClick(data.text7.caseTitle);
+        view.actionButtonsClick(data.text14.caseTitle);
         // View Details
         view.viewDetailsMenuButton();
         // Document Tab
         view.viewDetailsDocumentTab();
         // Check Selected Document Name Appear
-        view.viewDetailsCheckSelectedDocumentNameAppear(checkSelectedDocuments);
+        view.viewDetailsCheckSelectedDocumentNameAppear(data.text14.document1);
         // Add New + Tab
         view.addNewTab();
         // Check Selected Document Name Appear
-        view.viewDetailsAddExistingCheckSelectedDocumentNameAppear(checkSelectedDocuments);
-        // Check UnSelected Document Name Appear
-        view.viewDetailsAddExistingCheckUnSelectedDocumentNameAppear(checkUnSelectedDocuments);
+        view.viewDetailsAddExistingCheckSelectedDocumentNameAppear(data.text14.document1);
         // Team Member and Client Tab
         view.viewDetailsTeamMemberAndExternalCounselTab();
         //Selected Team Member
@@ -754,14 +765,15 @@ describe("Create Legal Matter Scenario 3",()=>{
         view.viewDetailsCheckSelectedTeamMemberOrClientNameAppear(checkSelectedClient);
         create.scrollUp();
         view.viewTab();
-        view.viewMatterSearchBox(data.text7.caseTitle);
-        view.actionButtonsClick(data.text7.caseTitle);
+        view.viewMatterSearchBox(data.text14.caseTitle);
+        view.actionButtonsClick(data.text14.caseTitle);
         // Edit Matter Info
         view.editMatterInfoMenuButton();
-        view.verifyEditMatterInfoCaseTitle(data.text7.caseTitle);
-        view.verifyEditMatterInfoCaseNumber(data.text7.caseNumber);
+        view.verifyEditMatterInfoCaseTitle(data.text14.caseTitle);
+        view.verifyEditMatterInfoCaseNumber(data.text14.caseNumber);
+        view.verifyEditMatterInfoDescription(data.text14.Description);
         view.editMatterInfoCancelButton();
-        view.actionButtonsClick(data.text7.caseTitle);
+        view.actionButtonsClick(data.text14.caseTitle);
         // Update Department
         view.updateDepartmentMenuButton();
         view.updateDepartmentCheckSelectedDepartmentName(selectDepartments);
